8d077753ebaebdc2a8d1b4736199d8a50dedfa7b
Devesh singh Verma <deveshsinghverma16@gmail.com>
2021-12-11 22:36:08 +0530

    Interview Bit Solution Java AddOneToNumber

---
+++ AddOneToNumber.java
@@ -0,0 +1,68 @@
+import java.util.*;
+
+import java.util.List.*;
+class AddOneToNumber{
+
+
+	public ArrayList<Integer> plusOne(ArrayList<Integer> A) {
+		ArrayList<Integer> B = new ArrayList<Integer>();
+
+		
+		int end = A.size()-1;
+		int carry =0;
+
+		while(end>=0){
+			int sum = 0;
+			if(end==A.size()-1){
+
+				sum = A.get(end)+1;
+
+			}
+			else{
+				sum = A.get(end);
+			}
+
+			sum = sum+carry;
+
+			if(sum>=10){
+
+				carry =1;
+
+			}else{
+				carry =0;
+			}
+		
+			B.add(sum%10);
+			end--;
+		}
+
+		// for(int i =0;i<B.length;i++){
+
+		// 	System.out.print(B[i]);
+		// }
+		B.add(carry);
+		
+
+		while(B.get(B.size()-1)==0){
+			B.remove(B.size()-1);
+		}
+		
+
+		Collections.reverse(B);
+		return B;
+
+
+	}
+
+	public static void main(String[] args) {
+		
+		ArrayList<Integer> A = new ArrayList<Integer>();
+
+		A.add(0);
+		A.add(2);
+		A.add(3);
+		AddOneToNumber addOneToNumber = new AddOneToNumber();
+		System.out.println(addOneToNumber.plusOne(A));
+	}
+
+}
